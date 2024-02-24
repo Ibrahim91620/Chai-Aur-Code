2ceb9ecd82b61044286678e49a247c5d8b645129
Md Ibrahim <faiziibrahim321@gmail.com>
2024-02-24 16:08:59 +0530

    operation

---
+++ operation.js
@@ -0,0 +1,21 @@
+let value=3
+let nagValue=-value;
+console.log(nagValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1="hello";
+// let str2=" ibrahim"
+
+// let str3=str1+str2;
+// console.log(str3);
+
+// 
+console.log(+true);
+console.log(+false);
+
+
