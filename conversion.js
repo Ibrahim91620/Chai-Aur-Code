06a59bafab328a8e54ae0adb6075d35b4fff7554
Md Ibrahim <faiziibrahim321@gmail.com>
2024-02-25 21:53:09 +0530

    datatypes

---
+++ conversion.js
@@ -0,0 +1,19 @@
+let score="12";
+console.log(typeof score);
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+
+let firstName="ibrahim"
+// slice(0,4);
+console.log(firstName);
+
+let isLogged="";
+let booleanisLoggedIn=Boolean(isLogged);
+console.log(booleanisLoggedIn);
+//"ibrahim"=true
+//
+let someNumber=12;
+let StringNumber=String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
