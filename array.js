cdb3b8f7b78e2400711bbd4a88b008fc66d67b3d
Md Ibrahim <faiziibrahim321@gmail.com>
2024-02-28 18:46:25 +0530

    array

---
+++ array.js
@@ -0,0 +1,19 @@
+const myArr=[1,2,3,4,5,6,"hitesh"];
+
+console.log(myArr[0]);
+
+const myHeroes=["shaktimaan","krrish"]
+
+// const myArr2=new myArr(1,2,3,4)
+// console.log(myArr2);
+
+myArr.push(4);
+console.log(myArr);
+myArr.pop();
+
+console.log(myArr);
+
+myArr.shift();//shifting from starting
+
+
+console.log(myArr);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log(myDate.toDateString());
+// console.log(myDate.toTimeString())
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.getDate());
+// console.log(typeof myDate);
+// let myCreatedDate= new Date(2024,0,23);
