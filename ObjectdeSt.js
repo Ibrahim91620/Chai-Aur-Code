24f6899bdc62492ef38e8627604bafba05980178
Md Ibrahim <faiziibrahim321@gmail.com>
2024-03-06 13:03:32 +0530

    json

---
+++ ObjectdeSt.js
@@ -0,0 +1,27 @@
+const course={
+    coursename: "js in hindi",
+    price:"999",
+    courseInstructor: "hitesh",
+}
+
+const {courseInstructor}=course
+console.log(courseInstructor);
+
+//{} it show destructuring
+
+
+//API it used to deploy own work to other
+{ //if we write in these curly braces it behave as json
+   "name":"ibrahim",
+   "courseName": "Js in Hindi",
+   "price": "free"  /// in json keys are also have braces.
+
+
+}
+[
+
+    {},
+    {},
+]
+
+//in api we write json
